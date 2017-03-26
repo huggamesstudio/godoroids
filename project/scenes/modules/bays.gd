1295a8eb9418ded0ad10c25c934343a7b7596d0b
Ricardo Gomez Escalante <ricardogomezescalante@gmail.com>
2017-03-26 21:14:58 +0200

    Mothership ejection fixed

--- project/scenes/modules/bays.gd
+++ project/scenes/modules/bays.gd
@@ -11,9 +11,10 @@ func eject_fighter():
 	# Load and instance the player ship
 	var fighter = load("res://scenes/entities/ship.tscn").instance()
 	_head.get_parent().add_child(fighter)
-	fighter.set_pos(_head.get_pos()+Vector2(0, -100))
-	fighter.get_node("BodyPhysics").speed_break(-10)
+	fighter.set_pos(_head.get_pos()+100*Vector2(-sin(_head.get_rot()), -cos(_head.get_rot())))
 	fighter.set_rotd(_head.get_rotd()+90)
+	fighter.get_node("BodyPhysics").set_speed(_head.get_node("BodyPhysics").get_speed())
+	fighter.get_node("BodyPhysics").speed_impulse(2)
 	
 	# Load player 1 behavior (input) into the ship
 	var player1_int = _head.get_node("Player1Input")
