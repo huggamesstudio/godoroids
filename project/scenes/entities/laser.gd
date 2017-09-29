b20d6154c9a0b35e79e51c86471157128697f25c
Ricardo Gomez Escalante <ricardogomezescalante@gmail.com>
2017-09-29 18:03:59 +0200

    Properties head of laser now goes on head

--- project/scenes/entities/laser.gd
+++ project/scenes/entities/laser.gd
@@ -1,7 +1,6 @@
 extends Node
 
 
-var _head
 var _physics
 
 var BASE_SPEED = 10
@@ -10,24 +9,23 @@ var _life_ticks = 60
 
 
 func _ready():
-	_head = get_parent()
-	_physics = _head.get_node("BodyPhysics")
+	_physics = get_node("BodyPhysics")
 	
 	set_fixed_process(true)
-	var rotation = _head.get_rot()
+	var rotation = get_rot()
 	_physics.change_speed(Vector2(cos(rotation), -sin(rotation))*BASE_SPEED)
 	
 func _fixed_process(delta):
-	var colliders = _head.get_overlapping_bodies()
+	var colliders = get_overlapping_bodies()
 	for collider in colliders:
 		if collider.has_method("hurt"):
 			collider.hurt(BASE_DAMAGE)
 		var explosion = load("res://scenes/animations/laser_explosion_anim.tscn").instance()
 		explosion.set_scale(Vector2(0.4, 0.4))
-		explosion.set_pos(_head.get_pos())
-		_head.get_parent().add_child(explosion)
-		_head.queue_free()
+		explosion.set_pos(get_pos())
+		get_parent().add_child(explosion)
+		queue_free()
 	
 	_life_ticks -= 1
 	if _life_ticks < 1:
-		_head.queue_free()
+		queue_free()
