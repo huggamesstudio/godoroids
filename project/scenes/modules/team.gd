17249727552b4ab70d573340b8b3d1fc8720ee51
Ricardo Gomez Escalante <ricardogomezescalante@gmail.com>
2017-10-29 11:26:48 +0100

    No longer tries to remove node from team group before adding it
    Bugfix: on ready,  team node was trying to remove the node from the
    corresponding team group before it was there

--- project/scenes/modules/team.gd
+++ project/scenes/modules/team.gd
@@ -1,22 +1,23 @@
 extends Node
 
 var _head
-
-
-export var _team = 0
+export var _team_number = 0
 
 
 func _ready():
 	_head = get_parent()
-	set_team(_team)
+	_apply_team(_team_number)
 
-func set_team(team):
-	_head.remove_from_group(Global.TEAM[_team])
-	_team = team
-	_head.add_to_group(Global.TEAM[_team])
+func set_team(team_idx):
+	_head.remove_from_group(Global.TEAM[_team_number])
+	_apply_team(team_idx)
+
+func _apply_team(team_idx):
+	_team_number = team_idx
+	_head.add_to_group(Global.TEAM[_team_number])
 	if _head.has_node("Sprite"):
 		var sprite = _head.get_node("Sprite")
-		sprite.set_modulate(Global.TEAM_COLORS[_team])
+		sprite.set_modulate(Global.TEAM_COLORS[_team_number])
 
 func get_team():
-	return _team;
+	return _team_number
